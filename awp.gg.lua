1f54c8e33bd78d60e987ce2c75f8fb541445cd8a
BTDCBUUS <107370285+BTDCBUUS1@users.noreply.github.com>
2025-04-20 19:26:01 +0100

    Create awp.gg.lua

---
+++ awp.gg.lua
@@ -0,0 +1,2 @@
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Rendered.Generic.Event.Display.TouchInterest, true)
+firetouchinterest(game.Players.LocalPlayer.Character.HumanoidRootPart, game:GetService("Workspace").Rendered.Generic.Event.Display.TouchInterest, false)
